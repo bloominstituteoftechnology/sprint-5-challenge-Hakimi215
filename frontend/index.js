b90602ade5e60245867dfe21c6651c3f079e82b3
Mirwes Hakimi <mirwes215@gmail.com>
2024-01-24 10:36:04 -0800

    fetching user and mentor data

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,18 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
+  try {
+    /// Fetch user and mentor data concurrently
+    const [userData, mentorData] = await Promise.all([
+      axios.get('http://localhost:3003/api/learners'),
+      axios.get('http://localhost:3003/api/mentors')
+    ]);
+
+  
+} catch (err) {
+  /// log error if fetching data fails
+  console.error('Error fetching data: ', err.message);
+}
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
